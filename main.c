209a21d4537d42b601440edd3788c0adba62671b
sally5670 <144642113+sally5670@users.noreply.github.com>
2023-11-02 11:31:44 +0900

    3-2

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 #define SIZE 5
 int main(void)
 {
 	int i;
-	int grade[SIZE];
-	int score[SIZE];
+	int a[SIZE] = {1, 2, 3, 4, 5};
+	int b[SIZE] = {1, 2, 3, 4, 5};
+	int flag=0;
 	for (i=0; i<SIZE; i++)
-		grade[i] = rand() % 100;
-	
-	for(i=0;i<SIZE;i++)
-		score[i]=grade[i];
-	
-	for( i=0; i<SIZE; i++)
-		printf("score[%d] = %d (grade:%i)\n", i, score[i],grade[i]);
+	{
+		if(a[i]!=b[i]){
+			printf("arrat a and b are not the same\n");
+			flag=1;
+			}
+		
+	}
+	if(flag==0)
+		printf("array a and b are the same\n");
+	system("PAUSE");
 	return 0;
 }
-
